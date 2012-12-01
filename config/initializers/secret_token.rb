20af75ce36be4be9ca22863ce6325927ca9d7e70
naoyamakino <naoyamakino@gmail.com>
2012-12-01 15:00:54 -0800

    init rails new openVancouver with mysql

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OpenVancouver::Application.config.secret_token = '0cb10280133437f1464422a884e9fa0ce1219699711e60507fcb7689a06a8fbd05b13e36657df8d06bdfb9538b15a532f50cd89c77f859cbeafae9ad0f99561d'
